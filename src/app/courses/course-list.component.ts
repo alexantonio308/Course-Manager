253931b0940732395b90e7cd93a4ebe6b7e0f36e
AlexAntonio <alexantonio308@gmail.com>
2020-11-30 12:08:57 -0300

    Pipes

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -6,13 +6,28 @@ import { CourseService } from './course.service';
   templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit{
-  courses: Course[] = [];
+  filteredCourses: Course[] = [];
+  // tslint:disable-next-line:variable-name
+  _courses: Course[] = [];
+  // tslint:disable-next-line:variable-name
+  _filterBy: string;
+
   constructor(private courseService: CourseService) {}
 
   ngOnInit(): void {
-    this.courses = this.courseService.retrieveAll();
+    this._courses = this.courseService.retrieveAll();
+    this.filteredCourses = this._courses;
   }
 
+  set filter(value: string){
+    this._filterBy = value;
+    // tslint:disable-next-line:max-line-length
+    this.filteredCourses =  this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+  }
+  // tslint:disable-next-line:typedef
+  get filter() {
+    return this._filterBy;
+  }
 }
 
 
