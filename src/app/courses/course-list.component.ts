f240e5e78047f55c858e4772c3e5a33984364581
AlexAntonio <alexantonio308@gmail.com>
2020-12-01 17:38:19 -0300

    Rotas

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
 import { CourseService } from './course.service';
+
 @Component({
-  selector: 'app-course-list',
-  templateUrl: './course-list.component.html'
+  templateUrl: './course-list.component.html',
 })
 export class CourseListComponent implements OnInit{
   filteredCourses: Course[] = [];
